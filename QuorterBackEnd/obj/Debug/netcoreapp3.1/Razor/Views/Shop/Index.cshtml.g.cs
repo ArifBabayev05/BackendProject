00ea7b53b89434eb8c09b9ddc2b021536f767c54
ArifBabayev05 <arifrb@code.edu.az>
2022-07-18 17:36:37 +0400

    Create Admin Dashboard

--- QuorterBackEnd/obj/Debug/netcoreapp3.1/Razor/Views/Shop/Index.cshtml.g.cs
+++ QuorterBackEnd/obj/Debug/netcoreapp3.1/Razor/Views/Shop/Index.cshtml.g.cs
@@ -13,14 +13,14 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
 #nullable restore
-#line 2 "/Users/babayev_/Desktop/Quorter2/QuorterBackEnd/Views/_ViewImports.cshtml"
+#line 4 "/Users/babayev_/Desktop/Quorter2/QuorterBackEnd/Views/_ViewImports.cshtml"
 using QuorterBackEnd;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/babayev_/Desktop/Quorter2/QuorterBackEnd/Views/_ViewImports.cshtml"
+#line 5 "/Users/babayev_/Desktop/Quorter2/QuorterBackEnd/Views/_ViewImports.cshtml"
 using QuorterBackEnd.Models;
 
 #line default
@@ -34,7 +34,7 @@ using DataEntities.Concrete;
 #line hidden
 #nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"13e184188bd1e68a1ba229449413f8f6e9bf48d2", @"/Views/Shop/Index.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"406e341eedf17b8298e0c20fed11e68805e0d2e9", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7f43b338c0a05b8d70d89971383b9850445251b3", @"/Views/_ViewImports.cshtml")]
     #nullable restore
     public class Views_Shop_Index : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<List<Feature2>>
     #nullable disable
