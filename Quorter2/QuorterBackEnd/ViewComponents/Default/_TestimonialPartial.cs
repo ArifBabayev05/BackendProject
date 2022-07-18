6445e5c1c0f7b468e27469487ecb42a0a635602e
ArifBabayev05 <arifrb@code.edu.az>
2022-07-18 15:38:35 +0400

    Finish Register

---
+++ Quorter2/QuorterBackEnd/ViewComponents/Default/_TestimonialPartial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Business.Concrete;
+using DAL.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuorterBackEnd.ViewComponents.Default
+{
+    public class _TestimonialPartial : ViewComponent
+    {
+        TestimonialManager testimonial = new TestimonialManager(new EfTestimonialDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = testimonial.TGetList();
+            return View(values);
+        }
+    }
+}
+
