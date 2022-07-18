cfed93303a858f18acd469db5574476f778f9b1d
ArifBabayev05 <arifrb@code.edu.az>
2022-07-18 19:43:27 +0400

    Finish Dashboard View

---
+++ QuorterBackEnd/Areas/Member/Controllers/ShopController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Concrete;
+using DAL.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuorterBackEnd.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class ShopController : Controller
+    {
+
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var values = featureManager.TGetList();
+            return View(values);
+        }
+    }
+}
+
