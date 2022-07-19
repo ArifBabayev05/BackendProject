c176321e1879b2eafc1ad54aadbae68520aef66e
ArifBabayev05 <arifrb@code.edu.az>
2022-07-19 23:16:48 +0400

    Fix Partials

---
+++ DAL/Migrations/20220719185531_AboutHero.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class AboutHero : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Add2",
+                table: "Abouts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Add3",
+                table: "Abouts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Add4",
+                table: "Abouts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Add2",
+                table: "Abouts");
+
+            migrationBuilder.DropColumn(
+                name: "Add3",
+                table: "Abouts");
+
+            migrationBuilder.DropColumn(
+                name: "Add4",
+                table: "Abouts");
+        }
+    }
+}
