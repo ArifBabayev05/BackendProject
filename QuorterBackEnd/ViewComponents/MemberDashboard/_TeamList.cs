89d09b4dba4e28b2c74b907429f9c8b9a3910479
ArifBabayev05 <arifrb@code.edu.az>
2022-07-20 23:37:23 +0400

    Fix Dashboard Appearnace

---
+++ QuorterBackEnd/ViewComponents/MemberDashboard/_TeamList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Business.Concrete;
+using DAL.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuorterBackEnd.ViewComponents.MemberDashboard
+{
+    public class _TeamList : ViewComponent
+    {
+        TeamManager team = new TeamManager(new EfTeamDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = team.TGetList();
+            return View(values); 
+        }
+    }
+}
+
