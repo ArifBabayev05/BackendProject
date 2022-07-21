522f54b50e1fdc793a25a1026fce7054ae1253be
ArifBabayev05 <arifrb@code.edu.az>
2022-07-21 18:22:21 +0400

    Add

---
+++ Business/ValidationRules/BlogValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DataEntities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules
+{
+    public class BlogValidator : AbstractValidator<Feature2>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Please Enter Title");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Please Enter Description");
+            RuleFor(x => x.MainPhoto).NotEmpty().WithMessage("Please Enter Photo");
+            RuleFor(x => x.Title).MaximumLength(50).WithMessage("Please Enter Title Less Than 50 Characters");
+            RuleFor(x => x.Title).MinimumLength(4).WithMessage("Please Enter Title More Than 5 Characters");
+
+
+
+
+        }
+    }
+}
+
--- QuorterBackEnd/Controllers/EmailController.cs
+++ QuorterBackEnd/Controllers/EmailController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
 using System.Threading.Tasks;
+using Business.Concrete;
+using DAL.EntityFramework;
 using DataEntities.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,33 +14,35 @@ namespace QuorterBackEnd.Controllers
 {
     public class EmailController : Controller
     {
-        [HttpGet]
+        EmailManager manager = new EmailManager(new EfEmailDal());
+        //[HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var values = manager.TGetList();
+            return View(values);
         }
 
-        [HttpPost]
-        public IActionResult Index(Email email)
-        {
-            string to = email.To;
-            string subject = email.Subject;
-            string body = email.Body;
-            MailMessage mm = new MailMessage();
-            mm.To.Add(to);
-            mm.Subject = subject;
-            mm.Body = body;
-            mm.From = new MailAddress("arif.babayev.2005@gmail.com");
-            mm.IsBodyHtml = false;
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
-            smtp.Port = 587;
-            smtp.UseDefaultCredentials = false;
-            smtp.EnableSsl = true;
-            smtp.Credentials = new System.Net.NetworkCredential("arif.babayev.2005@gmail.com", "password");
-            smtp.Send(mm);
-            ViewBag.message = "Thank You For Subscribing";
-            return View();
-        }
+        //[HttpPost]
+        //public IActionResult Index(Email email)
+        //{
+        //    string to = email.To;
+        //    string subject = email.Subject;
+        //    string body = email.Body;
+        //    MailMessage mm = new MailMessage();
+        //    mm.To.Add(to);
+        //    mm.Subject = subject;
+        //    mm.Body = body;
+        //    mm.From = new MailAddress("arif.babayev.2005@gmail.com");
+        //    mm.IsBodyHtml = false;
+        //    SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+        //    smtp.Port = 587;
+        //    smtp.UseDefaultCredentials = false;
+        //    smtp.EnableSsl = true;
+        //    smtp.Credentials = new System.Net.NetworkCredential("arif.babayev.2005@gmail.com", "password");
+        //    smtp.Send(mm);
+        //    ViewBag.message = "Thank You For Subscribing";
+        //    return View();
+        //}
     } 
 }
 
