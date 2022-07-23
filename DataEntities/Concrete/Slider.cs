ffd1078eec1846dbfafe91ee90d58ae09176f059
ArifBabayev05 <arifrb@code.edu.az>
2022-07-23 15:22:35 +0400

    Comments to Admin

--- DataEntities/Concrete/Slider.cs
+++ DataEntities/Concrete/Slider.cs
@@ -7,8 +7,8 @@ namespace DataEntities.Concrete
     {
         [Key]
         public int ID { get; set; }
-        public string Name { get; set; }
         public string Image { get; set; }
+        public string Name { get; set; }
 
     }
 }
